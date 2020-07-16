1e20016849c26c556a2a8b9a54449efc19440a20
Jasper <jasper@ngita.co.nz>
2020-07-16 13:57:38 +1200

    Wall now is shocking

---
+++ Assets/UnityVrScripts/wallHitDetector.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityVRScripts {
+    public class wallHitDetector : MonoBehaviour {
+        // Start is called before the first frame update
+        public ArdCom ardComManager;
+
+        void OnCollisionEnter(Collision collision) {
+            var tag = collision.gameObject.tag;
+            if (tag == "RightController") {
+                ardComManager.RightRelayOn();
+            }
+            else if (tag == "LeftController") {
+                ardComManager.LeftRelayOn();
+            }
+            else {
+                Debug.Log("Collision detected but not correct tag");
+            }
+        }
+        void OnCollisionExit(Collision collision) {
+            var tag = collision.gameObject.tag;
+            if (tag == "RightController") {
+                ardComManager.RightRelayOff();
+            }
+            else if (tag == "LeftController") {
+                ardComManager.LeftRelayOff();
+            }
+            else {
+                Debug.Log("Collision detected but not correct tag");
+            }
+        }
+    }
+}
